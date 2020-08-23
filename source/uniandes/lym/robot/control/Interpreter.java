a82e2164154602f29042378524407e37409965e8
Santiago <santiagomorafelix@gmail.com>
2020-08-23 15:49:08 -0500

    Cambiado el process()

--- source/uniandes/lym/robot/control/Interpreter.java
+++ source/uniandes/lym/robot/control/Interpreter.java
@@ -14,47 +14,47 @@ import uniandes.lym.robot.kernel.*;
  */
 
 public class Interpreter   {
-	
+
 	/**
 	 * Robot's world
 	 */
 	private RobotWorldDec world;   
-	
+
 	private class Tupla {
-		
+
 		String nombre;
 		int cant;
-		
+
 		public Tupla(String nombre){
 			this.nombre = nombre;
 		}
-		
+
 		void sumar(int cantidad) {
 			cant += cantidad;
 		}
 	}
-	
+
 	private ArrayList<Tupla> tuplas;
-	
+
 	public Interpreter()
-	  {
+	{
 		tuplas = new ArrayList<Tupla>();
-	  }
+	}
 
 
-    /**
+	/**
 	 * Creates a new interpreter for a given world
 	 * @param world 
 	 */
 
 
 	public Interpreter(RobotWorld mundo)
-      {
+	{
 		this.world =  (RobotWorldDec) mundo;
-		
-	  }
-	
-	
+
+	}
+
+
 	/**
 	 * sets a the world
 	 * @param world 
@@ -63,11 +63,11 @@ public class Interpreter   {
 	public void setWorld(RobotWorld m) 
 	{
 		world = (RobotWorldDec) m;
-		
+
 	}
-	  
-	
-	
+
+
+
 	/**
 	 *  Processes a sequence of commands. A command is a letter  followed by a ";"
 	 *  The command can be:
@@ -76,59 +76,80 @@ public class Interpreter   {
 	 *  
 	 * @param input Contiene una cadena de texto enviada para ser interpretada
 	 */
-	
+
 	public String process(String input) throws Error
-     {   
-		
-		 
+	{   
+
+
 		StringBuffer output=new StringBuffer("SYSTEM RESPONSE: -->\n");	
-		
+		/**
 		int i;
-	   int n;
-	   boolean ok = true;
-	   n= input.length();
-	    
-	    i  = 0;
-	    try	    {
-	    while (i < n &&  ok) {
-	    	switch (input.charAt(i)) {
-	    		case 'M': world.moveForward(1); output.append("move \n");break;
-	    		case 'R': world.turnRight(); output.append("turnRignt \n");break;
-	    		case 'C': world.putChips(1); output.append("putChip \n");break;
-	    		case 'B': world.putBalloons(1); output.append("putBalloon \n");break;
-	    		case  'c': world.pickChips(1); output.append("getChip \n");break;
-	    		case  'b': world.grabBalloons(1); output.append("getBalloon \n");break;
-	    		default: output.append(" Unrecognized command:  "+ input.charAt(i)); ok=false;
-	          }
-	
-	    	  if (ok) {
-	    		 if  (i+1 == n)  { output.append("expected ';' ; found end of input; ");  ok = false ;}
-	    		 else if (input.charAt(i+1) == ';') 
-	    		 {
-	    			 i= i+2;
-	    			 try {
-	    			        Thread.sleep(1000);
-	    			    } catch (InterruptedException e) {
-	    			        System.err.format("IOException: %s%n", e);
-	    			    }
-	    			 
-	    		 }
-	    		 else {output.append(" Expecting ;  found: "+ input.charAt(i+1)); ok=false;
-	    	 }
-	    	 }
-	    
-	    
-	    }
-	    
-	    }
-	 catch (Error e ){
-	 output.append("Error!!!  "+e.getMessage());
-	 
- }
-	    return output.toString();
-	    }
+		int n;
+		boolean ok = true;
+		n= input.length();
+		i  = 0;
+		 **/
+		try	    {
+			if(input.startsWith("ROBOT_R")&&input.contains("BEGIN")&&input.endsWith("END")){
+				String dospartes = input.substring(7);
+				String[] loquehacer = dospartes.split("BEGIN");
+				String[] cmds;
+				if(dospartes.contains("VARS")) {
+					String lineaconvars = loquehacer[0].substring(4);
+					String[] nombresVars = lineaconvars.split(",");
+					for(String nombre: nombresVars) {
+						Tupla var = new Tupla(nombre);
+						tuplas.add(var);
+					}
+				}
+				cmds = loquehacer[1].split(";");
+				metodoCmds(cmds);
+			}
+			else throw new Exception("Unrecognized command");
+			output.append("Instructions executed.");
+		}
+		catch (Exception e ){
+			output.append("Error!!!  "+e.getMessage());
+		}
+		return output.toString();
+		/**
+			while (i < n &&  ok) {
+				switch (input.charAt(i)) {
+				case 'M': world.moveForward(1); output.append("move \n");break;
+				case 'R': world.turnRight(); output.append("turnRignt \n");break;
+				case 'C': world.putChips(1); output.append("putChip \n");break;
+				case 'B': world.putBalloons(1); output.append("putBalloon \n");break;
+				case  'c': world.pickChips(1); output.append("getChip \n");break;
+				case  'b': world.grabBalloons(1); output.append("getBalloon \n");break;
+				default: output.append(" Unrecognized command:  "+ input.charAt(i)); ok=false;
+				}
+
+				if (ok) {
+					if  (i+1 == n)  { output.append("expected ';' ; found end of input; ");  ok = false ;}
+					else if (input.charAt(i+1) == ';') 
+					{
+						i= i+2;
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							System.err.format("IOException: %s%n", e);
+						}
+
+					}
+					else {output.append(" Expecting ;  found: "+ input.charAt(i+1)); ok=false;
+					}
+				}
 
 
-	
+			}
+
+		}
+		 **/
+
+	}
+
+	private void metodoCmds(String[] cmds) {
+
+	}
+
 }
-	    
