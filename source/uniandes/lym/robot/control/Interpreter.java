1819d3eaf623ae68c1505fcbaee4f4ad08d730ca
Sergio Andres Guillen Fonseca <sa.guillen@uniandes.edu.co>
2020-08-23 19:26:04 -0500

    update 1

--- source/uniandes/lym/robot/control/Interpreter.java
+++ source/uniandes/lym/robot/control/Interpreter.java
@@ -95,14 +95,19 @@ public class Interpreter   {
 				String[] loquehacer = dospartes.split("BEGIN");
 				String[] cmds;
 				if(dospartes.contains("VARS")) {
-					String lineaconvars = loquehacer[0].substring(4);
+					String lineaconvars = loquehacer[0].substring(5);
 					String[] nombresVars = lineaconvars.split(",");
 					for(String nombre: nombresVars) {
+						String limp = nombre.replace(" ", "");
+						limp=limp.replaceAll("\n", "");
+						nombre = limp;
+						System.out.println(nombre);
 						Tupla var = new Tupla(nombre);
 						tuplas.add(var);
 					}
 				}
 				cmds = loquehacer[1].split(";");
+				//				System.out.println(cmds[0]);
 				metodoCmds(cmds);
 			}
 			else throw new Exception("Unrecognized command");
@@ -149,6 +154,113 @@ public class Interpreter   {
 	}
 
 	private void metodoCmds(String[] cmds) {
+
+		Tupla t = null;
+
+		for(String comando	: cmds)
+		{
+			String limp = comando.replace("\n", ""); //limpia saltos de linea y espacios
+			limp = limp.replace(" ", "");
+			comando = limp;
+
+
+			if (comando.endsWith("END"));
+			{
+				String quitar = comando.replace("END", "");
+				comando = quitar;
+
+			}
+			if (comando.contains("assign"))
+			{
+				comando = comando.substring(comando.indexOf("(")+1,comando.indexOf(")"));
+				String[] asignar = comando.split(",");
+				//				System.out.println(comando);
+				//				System.out.println("si entra");
+
+				int numero = Integer.parseInt(asignar[1]);
+
+				if(numero!=0)
+				{
+					boolean esVariable = false;
+
+					for(int i = 0; i<tuplas.size() && esVariable==false; i++)
+					{
+						if(tuplas.get(i).nombre.equals(asignar[0]))
+						{
+							tuplas.get(i).sumar(numero);
+							esVariable=true;
+							t = tuplas.get(i);
+						}
+					}
+					if(esVariable==true)
+					{
+						System.out.println(t.cant+"");
+					}else{
+						System.out.println("No es variable declarada");
+					}
+				}
+
+
+
+			}else if (comando.startsWith("move"))
+			{
+				int numero = 0;
+				comando= comando.substring(comando.indexOf("(")+1,comando.indexOf(")"));
+
+				boolean esVariable = false;
+
+				for(int i = 0; i< tuplas.size()&&esVariable==false; i++)
+				{
+					if(tuplas.get(i).nombre.equals(comando))
+					{
+						numero=tuplas.get(i).cant;
+						esVariable = true;
+					}
+				}
+				if(esVariable==false)
+				{
+					numero = Integer.parseInt(comando);
+				}
+				world.moveForward(numero);
+
+			}else if (comando.startsWith("turn"))
+			{
+				comando= comando.substring(comando.indexOf("(")+1,comando.indexOf(")"));
+				if(comando.equals("right"))
+				{
+					world.turnRight();
+				}
+				else if(comando.equals("left"))
+				{
+					world.turnRight();
+					world.turnRight();
+					world.turnRight();
+				}
+				else if(comando.equals("around"))
+				{
+					world.turnRight();
+					world.turnRight();
+				}
+				else{
+					System.out.println("No hay direccion");
+					}
+			}
+			//			else if( comando.startsWith(turn))
+//			Ejemplo
+
+//ROBOT_R
+//VARS a, b
+//BEGIN
+//assign(a ,3);
+//assign(b ,1);
+//turn(right);
+//move(b);
+//END
+			//System.out.println(comando);
+
+
+
+		}
 
 	}
 
